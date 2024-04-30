682baab4ee64ee3d1fffe91c15316acfce487139
Cameron Gelber <cam.gelber@gmail.com>
2024-04-30 16:09:22 -0700

    Updated Docstrings

--- main/physics-sim.py
+++ main/physics-sim.py
@@ -1,11 +1,22 @@
 import numpy as np
 
 class System2D():
-    def __init__(self, m_ball=1, m_robot=.1, r_ball=1, h_robot=.1):
+    def __init__(self, m_ball=1, m_robot=.1, r_ball=1, h_robot=.1, dt=.01):
+        """
+        Initializes a 2D system with default or provided parameters.
+
+        Parameters:
+        - m_ball (float): Mass of the ball. (kg)
+        - m_robot (float): Mass of the robot. (kg)
+        - r_ball (float): Radius of the ball. (m)
+        - h_robot (float): Height of the robot. (m)
+        - dt (float): Time step size (s)
+        """
         self.m_ball = m_ball
         self.m_robot = m_robot
         self.r_ball = r_ball
         self.h_robot = h_robot
+        self.dt = dt
         self.theta_robot = 0
         self.theta_dot_robot = 0
         self.theta_ddot_robot = 0
@@ -13,31 +24,53 @@ class System2D():
         self.theta_ddot = 0
 
     def reset_system_rand(self):
-        # Resets System to chosen system state with certain amount of randomness
-        # This method will be used to reset the system after a failed training loop
-        # This method will reset these variables:
-        # self.theta_robot = 0
-        # self.theta_dot_robot = 0
-        # self.theta_ddot_robot = 0
-        # self.theta_dot_ball = 0
-        # self.theta_ddot = 0
+        """
+        Resets the system to a random state.
+
+        This method resets the following variables:
+        - self.theta_robot
+        - self.theta_dot_robot
+        - self.theta_ddot_robot
+        - self.theta_dot_ball
+        - self.theta_ddot
+
+        This method is typically used after a failed training loop to restart with new conditions.
+        """
         pass
 
     def set_system(self):
-        # Sets system to stated system state
-        # This can be used to test trained system with various initial conditions
-        # This method will set these variables to selected values:
-        # self.theta_robot = 0
-        # self.theta_dot_robot = 0
-        # self.theta_ddot_robot = 0
-        # self.theta_dot_ball = 0
-        # self.theta_ddot = 0
+        """
+        Sets the system to a specified state.
+
+        This method sets the following variables to specific values:
+        - self.theta_robot
+        - self.theta_dot_robot
+        - self.theta_ddot_robot
+        - self.theta_dot_ball
+        - self.theta_ddot
+
+        This can be used for testing the system with different initial conditions.
+        """
         pass
 
     def step(self):
-        # This method will step the system forward in time given the current system state
+        """
+        Steps the system forward in time based on the current state.
+
+        This method calculates the next state of the system based on its current state
+        and any control inputs (not yet implemented).
+        """
         pass
 
     def set_control(self):
-        # This method will set the conrol variables given the current system state
+        """
+        Sets the control variables based on the current system state.
+
+        This method sets the following variables to specific values:
+        - self.theta_dot_robot
+        - self.theta_ddot_robot
+
+        This method will implement logic to set control inputs for the system based on
+        its current state. This is used to update control strategies during simulation.
+        """
         pass
