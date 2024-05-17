68db355c535186f924fd63ed40fb1108cdaf7076
Cameron <cgelber@uw.edu>
2024-05-17 12:13:02 -0700

    Started the RL part for the 2d sim, updated file names so they were importable

--- main/physics_sim.py
+++ main/physics_sim.py
@@ -19,9 +19,8 @@ class System2D():
         self.dt = dt
         self.theta_robot = 0
         self.theta_dot_robot = 0
-        self.theta_ddot_robot = 0
         self.theta_dot_ball = 0
-        self.theta_ddot = 0
+        self.theta_ddot_ball = 0
 
     def reset_system_rand(self):
         """
@@ -30,7 +29,6 @@ class System2D():
         This method resets the following variables:
         - self.theta_robot
         - self.theta_dot_robot
-        - self.theta_ddot_robot
         - self.theta_dot_ball
         - self.theta_ddot
 
@@ -45,7 +43,6 @@ class System2D():
         This method sets the following variables to specific values:
         - self.theta_robot
         - self.theta_dot_robot
-        - self.theta_ddot_robot
         - self.theta_dot_ball
         - self.theta_ddot
 
@@ -68,9 +65,50 @@ class System2D():
 
         This method sets the following variables to specific values:
         - self.theta_dot_robot
-        - self.theta_ddot_robot
 
         This method will implement logic to set control inputs for the system based on
         its current state. This is used to update control strategies during simulation.
         """
         pass
+
+    def get_observation_space(self):
+        """
+        Returns the observation space of the system as a numpy array.
+
+        The observation space includes the following parameters:
+        - theta_robot: Angle of the robot.
+        - theta_dot_robot: Angular velocity of the robot.
+        - theta_dot_ball: Angular velocity of the ball.
+        - theta_ddot_ball: Angular acceleration of the ball.
+
+        Returns:
+            numpy.ndarray: A numpy array containing the observation space parameters 
+                        [theta_robot, theta_dot_robot, theta_ddot_robot, theta_dot_ball, theta_ddot_ball].
+        
+        Example:
+            >>> obj = YourClass()
+            >>> observation_space = obj.get_observation_space()
+            >>> print(observation_space)
+            [theta_robot_value, theta_dot_robot_value, theta_ddot_robot_value, theta_dot_ball_value, theta_ddot_ball_value]
+        """
+        return np.array[self.theta_robot, self.theta_dot_robot,
+                        self.theta_dot_ball, self.theta_ddot_ball]
+    
+    def get_action_space(self):
+        """
+        Returns the action space of the system as a numpy array.
+
+        The action space is defined as the range of possible actions that can be 
+        taken by the system. In this case, it is a continuous range between -10 and 10.
+
+        Returns:
+            numpy.ndarray: A numpy array containing the lower and upper bounds of 
+                        the action space, i.e., [-10, 10].
+        
+        Example:
+            >>> obj = YourClass()
+            >>> action_space = obj.get_action_space()
+            >>> print(action_space)
+            [-10, 10]
+        """
+        return np.array([-10, 10])
